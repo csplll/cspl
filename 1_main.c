03c00a31114b6428acc9c147cbd961183a53d153
csplll <chenshipeng228@qq.com>
2025-10-21 17:20:49 +0800

    Update main.c
    Modified the program to read a float input and convert it from Celsius to Fahrenheit.

--- 1_main.c
+++ 1_main.c
@@ -1,9 +1,11 @@
-//
-//
-//
+//202510302231
+//chenshipeng228@qq.com
+//陈仕鹏
 #include <stdio.h>
 int main()
 {
-    printf("Hello World");
+    float a = 0;
+    scanf("%f",&a);
+    printf("%.1f",a*9.0/5.0+32.0);
     return 0;
 }
