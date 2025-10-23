1888cfd7901d1db846135651a7fe5021ec47dde6
csplll <chenshipeng228@qq.com>
2025-10-23 12:19:45 +0800

    Update main.c

--- 2_main.c
+++ 2_main.c
@@ -2,21 +2,27 @@
 //chenshipeng228@qq.com
 //陈仕鹏
 #include <stdio.h>
-int main()
-{
+int main(){
     int a;
-    scanf("%d",&a);
-    if(a>=90&&a<=100){
-        printf("A");
-    }else if(a>=80&&a<=89){
-        printf("B");
-    }else if(a>=70&&a<=79){
-        printf("C");
-    }else if(a>=60&&a<=69){
-        printf("D");
-    }else if(a>=0&&a<=59){
-        printf("E");
+    int b;
+    char c;
+    int x;
+    scanf("%d %d %c",&a,&b,&c);
+    if(c=='+'){
+        x=a+b;
+    }else if(c=='-'){
+        x=a-b;
+    }else if(c=='*'){
+        x=a*b;
+    }else if(c=='/'){
+        if(b==0){
+            printf("除数不能为零，请重新输入");
+            return 1;
+        }
+        x=a/b;
     }else{
-        printf("请输入0到100的数");
-    }
+        printf("请输入有效运算符");
+        return 1;
+    }printf("%d",x);
+    return 0;
 }
